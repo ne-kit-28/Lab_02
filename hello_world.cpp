ff4692ae4d8d03d233e6a94f237b05fb332513dc
ne-kit-28 <nickit.nic098@yandex.ru>
2023-03-08 17:34:26 +0300

    comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 int main(){
-	std::string a;
+	std::string a; //create a
 	std::cout << "Nickname" << std::endl;
-	std::cin >> a;
+	std::cin >> a; //vvod nickname
 	std::cout<<"Hello world from " << a;
 }
