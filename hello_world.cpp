fbd35735929556f89515f5718083344990f7798e
ne-kit-28 <nickit.nic098@yandex.ru>
2023-03-08 17:27:45 +0300

    Using was deleted

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main(){
-	string a;
-	cout << "Nickname" << endl;
-	cin >> a;
-	cout<<"Hello world from " << a;
+	std::string a;
+	std::cout << "Nickname" << std::endl;
+	std::cin >> a;
+	std::cout<<"Hello world from " << a;
 }
