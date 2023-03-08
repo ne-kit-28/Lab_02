6ab80c37b2e1da8fa87fd3a772da09c5105ca3b3
ne-kit-28 <nickit.nic098@yandex.ru>
2023-03-08 19:52:30 +0300

    errors done

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <string>
+<<<<<<< HEAD
 int main(){
 	std::string a; //create perem
 	std::cout << "Nickname" << std::endl;
 	std::cin >> a; //ввод имени
 	std::cout<<"Hello world from " << a;
+=======
+
+int
+main()
+{
+	std::string a;
+	std::cout << "Nickname" << std::endl;
+	std::cin >> a;
+	std::cout << "Hello world from " << a;
+>>>>>>> a7f1b99 (Changed code style)
 }
